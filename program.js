fd72a74f2aa0bbf926639cb07a51a508d6806416
colin <colinnguyen02@gmail.com>
2023-05-18 16:42:35 -0400

    update commit

--- program.js
+++ program.js
@@ -3,7 +3,6 @@ const PORT = argv[2]
 const express = require('express')
 const path = require('path')
 const app = express()
-const sharp = require('sharp');
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static('public'));
